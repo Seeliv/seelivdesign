22f8954d9fc041a1f54fecda50f92daf4a537666
kandy <kroy.iips@gmail.com>
2019-03-09 21:24:46 +0530

    setup

---
+++ prototype.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Untitled Document</title>
+<?php include("head.php") ?>
+</head>
+
+<body>
+<?php include("header.php"); ?>
+<main>
+<section class="side-bar">
+  <div class="side-bar-content">
+    
+  </div>
+</section>
+<div class="content">
+  <div class="content-data">
+  
+  </div>
+</div>
+</main>
+<?php include("footer.php"); ?>
+</body>
+</html>
