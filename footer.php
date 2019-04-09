2741dc58d86e45e5f45c3a12c05ec89b21192899
Abdul Aziz <dr@seeliv.com>
2019-04-09 08:49:43 +0800

    code updated

--- footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-<script src="js/jquery.min.js"></script>
-<script src="js/custum.js"></script>
-</footer>
--- head.php
+++
@@ -1,4 +0,0 @@
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
-<link rel="stylesheet" type="text/css" href="css/fontawesome/css/all.css" />
-<link rel="stylesheet" type="text/css" href="css/style.css" />
