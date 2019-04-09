a18fa45e59ce33d0200c921cd07fd83a1b67c98d
Abdul Aziz <dr@seeliv.com>
2019-04-09 08:53:31 +0800

    updated

---
+++ signUp.php
@@ -0,0 +1,67 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Untitled Document</title>
+<?php include("head.php") ?>
+</head>
+
+<body >
+<div class="top-log">
+  <div class="lang"><a href="#">English</a><a href="#">中文</a> </div>
+  <div class="log-pg-hds">Sign-Up</div>
+  <div class="log-pg-optn"><a href="#"><i class="fa fa-question"></i><strong>Support</strong></a></div>
+</div>
+<div class="usr-sign-up">
+ <div class="sign-up-wrp">
+  <div class="sign-up-sectn">
+    <div class="roll-fr-log" style="background: #25a2b8;"><i class="fa fa-users"></i></div>
+    <div class="log-fr-cnts">
+      <h2><i class="fa fa-users"></i>General Account</h2>
+      <p>General Account is <strong>Multi-functional</strong> account for Students, Teachers
+as well as for Local Users.</p>
+      <ul>
+      <li>After signed up, You will be able to instant log-in to your account.</li>
+<li>In case of providing wrong information, you will be requested to
+provide Personal documents for account verification.</li>
+<li>In case of providing fake information, your account can be blocked
+Temporarily or permanently.</li>
+
+      </ul>
+      <div>
+        <label class="chk-bx roll-chkbx"><input type="checkbox" /><i class="fa fa-check-square"></i></label>Agreed to our <a href="#">Terms</a>, <a href="#">Data Policy</a> & <a href="#">Cookies Policy</a>.
+
+      </div>
+      <a href="#" class="roll-bt">Sign-Up Now For Free</a>
+    </div>
+  </div>
+  <p>Already have an account? <a href="#" >Login</a></p>
+  </div>
+ <div class="sign-up-wrp">
+  <div class="sign-up-sectn">
+    <div class="roll-fr-log" style="background: #2ba745;"><i class="fa fa-bank"></i></div>
+    <div class="log-fr-cnts">
+      <h2><i class="fa fa-bank"></i>School Account</h2>
+      <p>School Account is <strong>Multi-functional</strong> account for Educational Institute, 
+School & Organizations.</p>
+      <ul>
+      <li>After signed up, You will be able to instant log-in to your account.</li>
+<li>In case of providing wrong information, you will be requested to
+provide Personal documents for account verification.</li>
+<li>In case of providing fake information, your account can be blocked
+Temporarily or permanently.</li>
+
+      </ul>
+      <div>
+        <label class="chk-bx roll-chkbx"><input type="checkbox" /><i class="fa fa-check-square"></i></label>Agreed to our <a href="#">Terms</a>, <a href="#">Data Policy</a> & <a href="#">Cookies Policy</a>.
+
+      </div>
+      <a href="#" class="roll-bt">Sign-Up Now For Free</a>
+    </div>
+  </div>
+  <p>Already have an account? <a href="#" >Login</a></p>
+  </div>
+</div>
+<?php include("footer.php"); ?>
+</body>
+</html>
