5067e6ea5bbc5c8dd636db83987c693778ce9243
Abdul Aziz <dr@seeliv.com>
2019-04-11 07:00:34 +0800

    i

--- Admin_update_header.php
+++ Admin_update_header.php
@@ -71,7 +71,7 @@
         <li><a href=""><i class="fa fa-compass"></i>Content</a></li>
         <li><a href="d-charges.php"><i class="fa fa-comment-dollar"></i>D-charges</a></li>
         <li><a href="P-charges.php"><i class="fa fa-search-dollar"></i>P-charges</a></li>
-        <li><a href="d-tax.php"><i class="fa fa-percent"></i>D-Tax</a></li>
+        <li><a href="d-tax.php"><i class="fa fa-percentage"></i>D-Tax</a></li>
         <li><a href="General-Account-Restriction.php"><i class="fa fa-ban"></i>Restriction</a></li>
         <li><a href=""><i class="fa fa-shield-alt"></i>Limitations</a></li>
       </ul>
