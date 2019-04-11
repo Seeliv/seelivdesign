27979707e7cc9d24f7b96dd835e9552960a9a255
Abdul Aziz <dr@seeliv.com>
2019-04-11 09:15:57 +0800

    old design

---
+++ forgot-password.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Untitled Document</title>
+<?php include("head.php") ?>
+</head>
+
+<body >
+<div class="top-log">
+  <div class="lang"><a href="#">English</a><a href="#">中文</a> </div>
+  <div class="log-pg-hds">Reset Password</div>
+  <div class="log-pg-optn"><a href="#"><i class="fa fa-question"></i><strong>Support</strong></a></div>
+</div>
+<div class="general-form">
+  <div class="form-field">
+    <div class="cnt-sort  for-mobile-mod active"><input type="text" /><div class="cnt-optn"></div></div>
+    <div class="for-email-mod"><input type="text" placeholder="Email / Account ID / Approval ID" /></div>
+  </div>
+  <div class="form-field">
+  <input type="text" placeholder="Enter ASP Pin" />
+  </div>
+  <p class="lnk-two">
+  <a href="#" class="send-log-mod">Reset Via Email / IDs</a>
+<a href="#">Forget ASP Pin</a>
+  </p>
+  <div class="form-field">
+    <input type="button" value="Next" class="roll-btn full-btn" />
+  </div>
+  <p><a href="#" >Return To Login</a></p>
+</div>
+<?php include("footer.php"); ?>
+</body>
+</html>
---
+++ resetASPPin4.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Untitled Document</title>
+<?php include("head.php") ?>
+</head>
+
+<body >
+<div class="top-log">
+  <div class="lang"><a href="#">English</a><a href="#">中文</a> </div>
+  <div class="log-pg-hds">Reset Password</div>
+  <div class="log-pg-optn"><a href="#"><i class="fa fa-question"></i><strong>Support</strong></a></div>
+</div>
+<div class="general-form">
+  <div class="form-field">
+  <input type="text" placeholder="Type 4 digit New ASP PIN" />
+  </div>
+  <div class="form-field">
+  <input type="text" placeholder="Retype 4 digit New ASP PIN" />
+  </div>
+  <div class="form-field">
+    <input type="button" value="Update ASP PIN" class="roll-btn full-btn" />
+  </div>
+  <p><a href="#" >Return To Login</a></p>
+</div>
+<?php include("footer.php"); ?>
+</body>
+</html>
