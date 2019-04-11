f65c30ac1867dd24ab1362bd2f1ac88bbe056b39
Abdul Aziz <dr@seeliv.com>
2019-04-11 23:17:17 +0800

    y

--- Programs.php
+++ Programs.php
@@ -19,7 +19,7 @@
                                 <li><a href="#"><span class="fa-li"><i class="fa fa-map-marker"></i></span>States</a></li>
                                 <li><a href="#"><span class="fa-li"><i class="fa fa-envelope"></i></span>Postal Address</a></li>
                                 <li><a href="#"><span class="fa-li"><i class="fa fa-subway" ></i></span>Subway Address</a></li>
-                                <li><a href="#"><span class="fa-li"><i class="fas fa-language"></i></span>Mother Tongue</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-microphone-alt"></i></span>Mother Tongue</a></li>
                                 <li><a href="#"><span class="fa-li"><i class="fas fa-compass"></i></span>Study Field</a></li>
                                 <li><a href="#"><span class="fa-li"><i class="fas fa-layer-group"></i></span>Study level</a></li>
                                 <li><a class="active" href="#"><span class="fa-li"><i class="fas fa-graduation-cap"></i></span>Programs</a></li>
