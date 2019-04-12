e47b921c0a45dd832c7626b89153cf0f0f018a2d
Abdul Aziz <dr@seeliv.com>
2019-04-12 18:32:53 +0800

    d-values

--- Admin_d_cover_Photo(cp).php
+++ Admin_d_cover_Photo(cp).php
@@ -7,7 +7,7 @@
 </head>
 
 <body>
-<?php include("Admin_d_Values_header.php"); ?>
+<?php include("Admin_d-values_header.php"); ?>
 <main>
 <section class="side-bar">
   <div class="side-bar-content">
