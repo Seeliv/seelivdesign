2d6e2941ac9d03aa716b4c017b9a8cf3d12e291c
Abdul Aziz <dr@seeliv.com>
2019-04-13 00:50:30 +0800

    u

--- Admin_d-values_header.php
+++ Admin_d-values_header.php
@@ -67,7 +67,7 @@
   </div>
   <div class="header-bottom">
     <ul class="sub-menu">
-        <li><a href=""><i class="fas fa-rss"></i>D-Content</a></li>
+        <li><a href=""><i class="fab fa-whmcs"></i>D-Content</a></li>
         <li><a href=""><i class="fas fa-users-cog"></i>D-Profile</a></li>
         <li><a href="d-charges.php"><i class="fa fa-comment-dollar"></i>D-charges</a></li>
         <li><a href="P-charges.php"><i class="fa fa-search-dollar"></i>P-charges</a></li>
