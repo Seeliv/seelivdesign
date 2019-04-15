7dea4d439b784143f57dc689e3eb6ad64fa23ffb
Abdul Aziz <dr@seeliv.com>
2019-04-15 23:37:04 +0800

    d

--- Admin_Verify_Account_Name.php
+++ Admin_Verify_Account_Name.php
@@ -101,9 +101,9 @@
          <div class="sort-option">
            <button class="sort-button roll-filter"><i class="fa fa-filter"></i></button>
            <ul>
-             <li><button class="sort-button active">All</button></li>
              <li><button class="sort-button">Review In Pending</button></li>
              <li><button class="sort-button">Review Completed</button></li>
+             <li><button class="sort-button">Overall Requests</button></li>
            </ul>
          </div>
          <div class="table-pagging">
