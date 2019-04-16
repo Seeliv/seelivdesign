67c0a83fb7992dfd35b6ba72f0fb5aeafe7ab6f0
Abdul Aziz <dr@seeliv.com>
2019-04-16 19:40:02 +0800

    f

---
+++ chat.php
@@ -0,0 +1,88 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Chat</title>
+<?php include("head.php") ?>
+</head>
+
+<body>
+<?php include("Chat_Header.php"); ?>
+<main>
+<section class="side-bar">
+  <div class="side-bar-content">
+    <div class="search-list-item"><input type="text" placeholder="Search here"></div>
+    <div class="sidebar-list">
+    <ul class="fa-ul">
+        <li><a href="#"><div class="icon-r"><img src="images/icon40.png" /></div><strong>Kamran Ali</strong></a></li>
+        <li><a href="#"><div class="icon-r"><img src="images/icon41.png" /></div><strong>Abdul Aziz</strong></a></li>
+        <li><a href="#"><div class="icon-r"><img src="images/icon44.png" /></div><strong>Ali Raza</strong></a></li>
+                          </ul>
+                          </div>
+  </div>
+</section>
+<div class="content">
+  <div class="content-data">
+  <div class="content-in">
+    <div class="content-fix-head"><h1>Kamran Ali</h1></div>
+    <div class="main-content">
+      <div class="card-body noticeboard">
+                <div class="header_message sec2wrap btnwap">
+                    <div class="card-wrap">
+                     <div class="card-message">
+                    <div class="message">
+                         <p>General Account Default Cp</p>
+                       </div>
+                  </div>
+                     <div class="card-option">
+                    <div class="sidebtn ">
+                         <ul>
+                        <li>
+                        <a href="">
+                          <label class="switch">
+                            <input type="checkbox">
+                            <span class="slider round"></span> </label>
+                          </a>
+                         </li>
+                        <li><a href="" class="refresh"><img src="images/icon14.png" class="iconback1"></a></li>
+                        <li><a href="" class="edit-btn"><img src="images/edit.png" class="iconback2"></a></li>
+                      </ul>
+                       </div>
+                  </div>
+                    </div>
+                  <div class="cover-picture"><img src="images/banner1.png" /></div>
+                   </div>
+                  <div class="header_message sec2wrap btnwap">
+                    <div class="card-wrap">
+                     <div class="card-message">
+                    <div class="message">
+                         <p>School Account Default Cp</p>
+                       </div>
+                  </div>
+                     <div class="card-option">
+                    <div class="sidebtn ">
+                         <ul>
+                        <li>
+                        <a href="">
+                          <label class="switch">
+                            <input type="checkbox">
+                            <span class="slider round"></span> </label>
+                          </a>
+                         </li>
+                        <li><a href="" class="refresh"><img src="images/icon14.png" class="iconback1"></a></li>
+                        <li><a href="" class="edit-btn"><img src="images/edit.png" class="iconback2"></a></li>
+                      </ul>
+                       </div>
+                  </div>
+                    </div>
+                  <div class="cover-picture"><img src="images/banner1.png" /></div>
+                   </div>
+              </div>
+    </div>
+  </div>
+  </div>
+</div>
+</main>
+<?php include("footer.php"); ?>
+</body>
+</html>
