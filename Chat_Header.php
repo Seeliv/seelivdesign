1f64e800185593a12c27e2296db29296334eff9c
Abdul Aziz <dr@seeliv.com>
2019-04-17 00:17:50 +0800

    f

--- Chat_Header.php
+++ Chat_Header.php
@@ -65,10 +65,10 @@
     <ul class="sub-menu">
         <li><a href=""><i class="fas fa-comment-dots"></i>Chat</a></li>
         <li><a href=""><i class="fas fa-users"></i>Contact</a></li>
-        <li><a href=""><i class="fas fa-calendar-alt"></i>Schedule</a></li>
+        <li><a href=""><i class="fa fa-calendar-check-o" aria-hidden="true"></i>Schedule</a></li>
         <li><a href=""><i class="fas fa-school"></i>Profile</a></li>
-        <li><a href=""><i class="fas fa-calculator"></i>Processing Session</a></li>
-        <li><a href=""><i class="fas fa-server"></i>Students’ Data</a></li>
+        <li><a href=""><i class="fab fa-fort-awesome-alt"></i>New Application</a></li>
+        <li><a href=""><i class="fas fa-server"></i>Data Center</a></li>
       </ul>
   </div>
 </header>
