7bee1acd16fd0b2e80876768246ae06a015fab1a
Abdul Aziz <dr@seeliv.com>
2019-04-16 19:56:14 +0800

    f

--- Chat_Header.php
+++ Chat_Header.php
@@ -30,16 +30,14 @@
       <div class="nn-MenuNav_Wrapper">
         <nav id="pnProductNav" class="nn-MenuNav">
           <div id="pnProductNavContents" class="nn-MenuNav_Contents"> 
-           <a href="#" class="nn-MenuNav_Link" aria-selected="true"><i class="fas fa-search"></i>Search</a>
-            <a href="#" class="nn-MenuNav_Link"><i class="fa fa-handshake-o" aria-hidden="true"></i>Verify</a>
-            <a href="#" class="nn-MenuNav_Link"><i class="fa fa-globe"></i>Review</a>
-            <a href="#" class="nn-MenuNav_Link"><i class="fa fa-eye"></i>Monitor</a>
-            <a href="#" class="nn-MenuNav_Link"><i class="fa fa-envelope-open"></i>Email</a>
-            <a href="#" class="nn-MenuNav_Link"><i class="fas fa-question-circle"></i>Support</a>
-            <a href="#" class="nn-MenuNav_Link"><i class="fa fa-plane" aria-hidden="true"></i>Booking</a>
-            <a href="#" class="nn-MenuNav_Link"><i class="fa fa-briefcase"></i>Finance </a>
-            <a href="#" class="nn-MenuNav_Link"><i class="fa fa-atom"></i>Update</a>
-            <a href="#" class="nn-MenuNav_Link"><i class="fa fa-home"></i>Home</a>
+            <a href="#" class="nn-MenuNav_Link"><i class="fas fa-search"></i>Verify</a>
+            <a href="#" class="nn-MenuNav_Link"><i class="fas fa-qrcode"></i>QR Code</a>
+            <a href="#" class="nn-MenuNav_Link"><i class="fas fa-bell"></i>Account</a>
+            <a href="#" class="nn-MenuNav_Link"><i class="fas fa-bell"></i>Study</a>
+            <a href="#" class="nn-MenuNav_Link"><i class="fas fa-bell"></i></i>Booking</a>
+            <a href="#" class="nn-MenuNav_Link"><i class="fas fa-bell"></i>Transaction</a>
+            <a href="#" class="nn-MenuNav_Link"><i class="fas fa-bell"></i>support</a>
+            <a href="#" class="nn-MenuNav_Link"><i class="fas fa-sign-out-alt"></i>Sign Out</a>
             <span id="pnIndicator" class="nn-MenuNav_Indicator"></span> </div>
         </nav>
         <button id="pnAdvancerLeft" class="pn-Advancer pn-Advancer_Left" type="button">
@@ -65,14 +63,8 @@
   </div>
   <div class="header-bottom">
     <ul class="sub-menu">
-        <li><a href=""><i class="fas fa-users"></i>Users</a></li>
+        <li><a href=""><i class="fas fa-sms"></i>Chat</a></li>
         <li><a href=""><i class="fas fa-print"></i>Document</a></li>
-        <li><a href=""><i class="fas fa-comments"></i>E-Chat</a></li>
-        <li><a href=""><i class="fas fa-address-card"></i>Resume</a></li>
-        <li><a href=""><i class="fas fa-business-time"></i>Jobs</a></li>
-        <li><a href=""><i class="fas fa-graduation-cap"></i>Scholarships</a></li>
-        <li><a href=""><i class="fas fa-book"></i>Programs</a></li>
-        <li><a href=""><i class="fas fa-school"></i>Schools</a></li>
       </ul>
   </div>
 </header>
