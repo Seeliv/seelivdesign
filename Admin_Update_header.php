2f801336f68ee362ca2a28824e2b3d521015d457
Abdul Aziz <dr@seeliv.com>
2019-04-18 05:09:04 +0800

    G

--- Admin_Update_header.php
+++ Admin_Update_header.php
@@ -74,7 +74,7 @@
         <li><a href="d-tax.php"><i class="fa fa-user-circle"></i>D-Account</a></li>
         <li><a href="General-Account-Restriction.php"><i class="fa fa-ban"></i>Restriction</a></li>
         <li><a href=""><i class="fa fa-shield-alt"></i>Limitations</a></li>
-        <li><a href=""><i class="fas fa-print"></i>Document</a></li>
+        <li><a href=""><i class="fas fa-print"></i>cDocument</a></li>
       </ul>
   </div>
 </header>
