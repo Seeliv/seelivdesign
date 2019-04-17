18c2819e65a1f1b441106b6f39b76e381bfb8e26
Abdul Aziz <dr@seeliv.com>
2019-04-18 00:46:23 +0800

    r

--- Admin_Verify_Account_Name.php
+++ Admin_Verify_Account_Name.php
@@ -159,9 +159,8 @@
             <ul>
               <li><a class="p-icn" href="#"><img src="images/ReplaceP.png" /></a></li>
               <li><a class="n-icn" href="#"><img src="images/ReplaceN.png" /></a></li>
-              <li><a class="n-icn" href="#"><img src="images/ReplaceN.png" /></a></li>
               <li><a class="imgs-icn" href="#"><img src="images/icon13.png" /></a></li>
-              <li><a class="chk-icn" href="#"><img src="images/verify.png" /></a></li>
+              <li><a class="n-icn" href="#"><img src="images/Verify.png" /></a></li>
               <li><a class="rmv-icn" href="#"><img src="images/Reject.png" /></a></li>
             </ul>
           </div>
