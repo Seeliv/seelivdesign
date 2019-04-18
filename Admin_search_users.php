7e6a16ae7c29c639e25e0f307ae8bf79a390f3ed
Abdul Aziz <dr@seeliv.com>
2019-04-18 20:03:24 +0800

    g

--- Admin_search_users.php
+++ Admin_search_users.php
@@ -95,7 +95,7 @@
            <ul>
                                 
                                                    <li><img src="images/icon19.png"><span>621 </span></li>
-                                                   <li><img src="images/icon17.png"><span>India  </span></li>
+                                                   <li><img src="images/icon25.png"><span>India  </span></li>
                                                    <li><img src="images/icon29.png"><span>Name of person </span></li>
                                                     <li><img src="images/icon20.png"><span>GH213433453324532343</span></li>
                                                      <li><img src="images/icon30.png"><span>Review In Pending</span> </li>
