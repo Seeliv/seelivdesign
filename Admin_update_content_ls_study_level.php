6e1b2ff8193d93a56a32601067afc6e71782e6fd
Abdul Aziz <dr@seeliv.com>
2019-04-18 09:43:49 +0800

    t

---
+++ Admin_update_content_ls_study_level.php
@@ -0,0 +1,154 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>update ls study level</title>
+<?php include("head.php") ?>
+</head>
+
+<body>
+<?php include("Admin_Update_header.php"); ?>
+<main>
+<section class="side-bar">
+  <div class="side-bar-content">
+    <div class="search-list-item"><input type="text" placeholder="Search here"></div>
+    <div class="sidebar-list">
+    <ul class="fa-ul">
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-microphone"></i></span>Accent</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-globe-americas"></i></span>Countries</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-map-marked-alt"></i></span>States</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-search-location"></i></span>Postal Address</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fa fa-subway" ></i></span>Subway Address</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-microphone-alt"></i></span>Mother Tongue</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-compass"></i></span>Study field</a></li>
+                                <li><a class="active" href="#"><span class="fa-li"><i class="fas fa-layer-group"></i></span>Ls study level</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-layer-group"></i></span>Fs study level</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-graduation-cap"></i></span>Programs</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-book"></i></span>Subjects</a></li>
+                          </ul>
+                          </div>
+  </div>
+</section>
+<div class="content">
+  <div class="content-data">
+  <div class="content-in">
+    <div class="content-fix-head">
+       <h1>Ls study level</h1>
+       <div class="filter-form">
+         <div class="form-fl form-3"><!--
+           <div class="mob-filter-head"><button id="filter-close"><i class="fa fa-arrow-left"></i></button><p>State Search</p></div>-->
+           <div class="t-b-one"><input type="text" placeholder="State id" ><button type="submit"><i class="fa fa-search"></i></button></div>
+           <div class="o-b-one">
+             <select>
+             <option>All ls study level</option>
+             <option>001</option>
+             <option>003</option>
+             <option>004</option>
+             <option>005</option>
+             <option>006</option>
+             <option>007</option>
+             <option>008</option>
+             <option>009</option>
+             <option>010</option>
+             <option>011</option>
+             <option>012</option>
+             <option>013</option>
+             <option>014</option>
+             <option>015</option>
+             <option>016</option>
+             <option>017</option>
+             <option>018</option>
+             <option>019</option>
+             <option>020</option>
+             <option>021</option>
+             <option>022</option>
+             <option>023</option>
+             <option>024</option>
+             <option>025</option>
+             <option>026</option>
+             <option>027</option>
+             <option>028</option>
+             <option>029</option>
+             <option>030</option>
+             </select>
+           </div>
+           <div class="t-b-one"><input type="text" placeholder="State Name" ><button type="submit"><i class="fa fa-search"></i></button></div>
+         </div>
+         <div class="form-usr-option">
+           <a href="#" class="add-opn">Add As New</a>
+           <a href="#" class="rew-opn">Review Selected</a>
+           <a href="#" class="del-opn">Delete Selected</a>
+         </div>
+       </div>   
+       <div class="table-options">
+         <div class="all-sel"><label class="chk-bx"><input type="checkbox" /><i class="fa fa-check-square"></i></label> <a id="edit-mod">Edit</a></div>
+         <div class="mob-page-tool">
+           <ul>
+             <li>
+               <button class="mob-fil-sh filt-click" id="filter-open"><i class="fa fa-search"></i></button>
+             </li>
+             
+             <li>
+               <button class="mob-fil-sh filt-click" id="fill-optn"><i class="fa fa-filter"></i></button>
+               <div class="filter-bt-bl"></div>
+             </li>
+             <li>
+               <button class="mob-fil-sh filt-click" id="fill-sort"><i class="fa fa-sort"></i></button>
+             </li>
+             <li>
+               <button class="mob-fil-sh filt-click" id="form-opt-open" ><i class="fa fa-cog"></i></button>
+             </li>
+           </ul>
+         </div>
+         <div class="sort-option">
+           <button class="sort-button roll-filter"><i class="fa fa-filter"></i></button>
+           <ul>
+             <li><button class="sort-button active">Overall Input</button></li>
+             <li><button class="sort-button">Review In Pending</button></li>
+             <li><button class="sort-button">Review Completed</button></li>
+           </ul>
+         </div>
+         <div class="table-pagging">
+           <a href="#">&lt;</a>
+           <a href="#">1</a>
+           <a href="#">2</a>
+           <a href="#">3</a>
+           <a href="#">&gt;</a>
+         </div>
+       </div>
+    </div>
+    <div class="main-content">
+      <div class="table-list country-table">
+        <div class="table-row">
+          <label class="chk-bx"><input type="checkbox" /><i class="fa fa-check-square"></i></label>
+          <div class="cnt-top">
+          <div class="country-name">
+           <div>Punjab</div>
+           <div><a class="fr-expnd"><i class="fa fa-angle-down"></i> <span>Show more</span></a></div>
+          </div>
+          </div>
+          <div class="cnt-btom tog-expand">
+           <div class="row-list">
+           <ul>
+                                
+                                                   <li><img src="images/icon19.png"><span>621 </span></li>
+                                                   <li><img src="images/icon25.png"><span>001</span></li>
+                                                   <li><img src="images/icon25.png"><span>India</span></li>
+                                                     <li><img src="images/icon30.png"><span>Review In Pending</span></li>
+                                                      <li><img src="images/icon10.png"><span>Kamran Ali </span></li>
+                                                       <li><img src="images/icon22.png"><span>GH213433453324532343</span></li>
+                                             </ul>
+                                             
+          <div class="row-option"><a href="#">Edit</a></div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</div>
+</main>
+<?php include("footer.php"); ?>
+</body>
+</html>
