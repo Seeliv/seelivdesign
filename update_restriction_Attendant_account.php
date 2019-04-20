c90a1c13eee5f4c126ea5e2e243abbd6416959ca
Abdul Aziz <dr@seeliv.com>
2019-04-21 04:28:54 +0800

    F

---
+++ update_restriction_Attendant_account.php
@@ -0,0 +1,106 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Attendant Account Restriction</title>
+<?php include("head.php") ?>
+</head>
+
+<body>
+<?php include("Admin_Update_header.php"); ?>
+<main>
+<section class="side-bar">
+  <div class="side-bar-content">
+    <div class="search-list-item lft-side-sort">
+    <div class="sort-option option-sort-3">
+           <ul>
+             <li><button class="sort-button">General Account</button></li>
+             <li><button class="sort-button">School Account</button></li>
+             <li><button class="sort-button active">Attendant Account</button></li>
+           </ul>
+         </div>
+     
+    <div class="sort-option">
+           <ul>
+             <li><button class="sort-button active">General</button></li>
+             <li><button class="sort-button">Country</button></li>
+             <li><button class="sort-button">State</button></li>
+           </ul>
+         </div>
+    <input type="text" placeholder="Search here"></div>
+    <div class="sidebar-list">
+    <ul class="fa-ul">
+                                <li><a class="active" href="#"><span class="fa-li"><i class="fas fa-user-plus"></i></span>New Account Sign-up</strong></a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-sign-in-alt"></i></span>Existing Account login</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-user-circle"></i></span>Changing Guardian</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-user-circle"></i></span>Being Guardian</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-user-md"></i></span>Being Coordinator</a></li>
+  </div>
+</section>
+<div class="content">
+  <div class="content-data">
+  <div class="content-in">
+    <div class="content-fix-head">
+       <h1>New Account Sign-up</h1>
+       <div class="filter-form">
+         
+       </div>   
+       
+    </div>
+    <div class="main-content">
+      <div class="acc-set-tog">
+        <h4>This Action Will be applicable to all Accounts</h4>
+        <div class="set-sectn">
+          <div class="set-sctn-row">
+            <div class="set-scn-lbl">Restrict</div>
+            <div class="set-action">
+                  <div class="sidebtn ">
+                    <ul>
+                    <li> <a href="">
+                        <label class="switch">
+                          <input type="checkbox">
+                          <span class="slider round"></span> </label>
+                        </a> </li>
+                    </ul>
+                  </div> 
+                  </div>
+          </div>
+          <div class="set-sctn-row">
+            <div class="set-scn-lbl">Single Action Required</div>
+            <div class="set-action">
+                  <div class="sidebtn ">
+                    <ul>
+                    <li> <a href="">
+                        <label class="switch">
+                          <input type="checkbox">
+                          <span class="slider round"></span> </label>
+                        </a> </li>
+                    </ul>
+                  </div> 
+                  </div>
+          </div>
+          <div class="set-sctn-row">
+            <div class="set-scn-lbl">Multiple Action Required</div>
+            <div class="set-action">
+                  <div class="sidebtn ">
+                    <ul>
+                    <li> <a href="">
+                        <label class="switch">
+                          <input type="checkbox">
+                          <span class="slider round"></span> </label>
+                        </a> </li>
+                    </ul>
+                  </div> 
+                  </div>
+          </div>
+          <div class="set-sctn-row-cntr">Verify Identity <a href="#">Edit</a></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</div>
+</main>
+<?php include("footer.php"); ?>
+</body>
+</html>
