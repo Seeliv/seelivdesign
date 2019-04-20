36fed7f4a59ed2c150b990531036adb387a11c8f
Abdul Aziz <dr@seeliv.com>
2019-04-21 04:04:37 +0800

    f

--- update_restriction_general_account.php
+++ update_restriction_general_account.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>General Account General Restriction</title>
+<title>General Account Restriction</title>
 <?php include("head.php") ?>
 </head>
 
