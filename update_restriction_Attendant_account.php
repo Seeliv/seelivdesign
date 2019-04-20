6d287eb4557e9f7d32f755f7930edc3372a9dc3e
Abdul Aziz <dr@seeliv.com>
2019-04-21 04:30:27 +0800

    d

--- update_restriction_Attendant_account.php
+++ update_restriction_Attendant_account.php
@@ -35,6 +35,63 @@
                                 <li><a href="#"><span class="fa-li"><i class="fas fa-user-circle"></i></span>Changing Guardian</a></li>
                                 <li><a href="#"><span class="fa-li"><i class="fas fa-user-circle"></i></span>Being Guardian</a></li>
                                 <li><a href="#"><span class="fa-li"><i class="fas fa-user-md"></i></span>Being Coordinator</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-wallet"></i></span>Top-up Fund</strong></a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-money-check-alt"></i></span>Withdraw Fund</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-comments-dollar"></i></span>National fund Transfer</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-comments-dollar"></i></span>International fund Transfer</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-hand-holding-usd"></i></span>National fund Collection</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-hand-holding-usd"></i></span>International fund Collection</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-user-plus"></i></span>Adding New Friends</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-podcast"></i></span>Broadcast Service</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-calendar-alt"></i></span>1-on-1 Chat Calendar Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-microphone"></i></span>1-on-1 Chat Microphone Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="far fa-images"></i></span>1-on-1 Chat Gallery Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-camera"></i></span>1-on-1 Chat Camera Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-print"></i></span>1-on-1 Chat Documents Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-comment-dots"></i></span>1-on-1 Chat Outgoing message</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-comment-dots"></i></span>1-on-1 Chat Incoming message</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-phone-volume"></i></span>1-on-1 Chat Outgoing Audio call</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-phone-volume"></i></i></span>1-on-1 Chat Incoming audio call</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-video"></i></span>1-on-1 Chat Outgoing video call</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-video"></i></span>1-on-1 Chat Incoming video call</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-heart"></i></span>1-on-1 Chat Favorite Sharing</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-map-marker-alt"></i></span>1-on-1 Chat Location Sharing</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-address-book"></i></span>1-on-1 Chat Contact Card Sharing</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-users"></i></span>Creating New Group</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-calendar-alt"></i></span>Group Chat Calendar Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-microphone"></i></span>Group Chat Microphone Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="far fa-images"></i></span>Group Chat Gallery Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-camera"></i></span>Group Chat Camera Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-print"></i></span>Group Chat Documents Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-comment-dots"></i></span>Group Chat Outgoing message</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-comment-dots"></i></span>Group Chat Incoming message</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-phone-volume"></i></span>Group Chat Outgoing Audio call</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-phone-volume"></i></i></span>Group Chat Incoming audio call</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-video"></i></span>Group Chat Outgoing video call</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-video"></i></span>Group Chat Incoming video call</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-heart"></i></span>Group Chat Favorite Sharing</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-map-marker-alt"></i></span>Group Chat Location Sharing</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-address-book"></i></span>Group Chat Contact Card Sharing</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-calendar-alt"></i></span>Enrollment Chat Calendar Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-microphone"></i></span>Enrollment Chat Microphone Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="far fa-images"></i></span>Enrollment Chat Gallery Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-camera"></i></span>Enrollment Chat Camera Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-print"></i></span>Enrollment Chat Documents Access</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-comment-dots"></i></span>Enrollment Chat Outgoing message</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-comment-dots"></i></span>Enrollment Chat Incoming message</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-phone-volume"></i></span>Enrollment Chat Outgoing Audio call</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-phone-volume"></i></i></span>Enrollment Chat Incoming audio call</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-video"></i></span>Enrollment Chat Outgoing video call</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-video"></i></span>Enrollment Chat Incoming video call</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-heart"></i></span>Enrollment Chat Favorite Sharing</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-heart"></i></span>Enrollment Location Sharing</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-address-book"></i></span>Enrollment Chat Contact Card Sharing</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-file"></i></span>Posting Resume & Applying for Jobs</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-graduation-cap"></i></span>Applying for scholarships</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-book"></i></span>Applying for Programs</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-poll"></i></span>Applying & attempting Exams</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-ticket-alt"></i></span>Ticket Booking</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-trash-alt"></i></span>Auto-Deleting Message</a></li>
   </div>
 </section>
 <div class="content">
