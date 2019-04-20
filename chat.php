e61985b28c82c9387e5e7ea4e071ddb2a0abcd5b
Abdul Aziz <dr@seeliv.com>
2019-04-21 04:56:08 +0800

    f

---
+++ chat.php
@@ -0,0 +1,93 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/> 
+<title>Chat</title>
+<?php include("head.php") ?>
+</head>
+
+<body>
+<?php include("Chat_Header.php"); ?>
+<main>
+<section class="side-bar">
+  <div class="side-bar-content">
+    <div class="search-list-item"><input type="text" placeholder="Search here"></div>
+    <div class="sidebar-list">
+    <ul class="fa-ul">
+                                <li><a class="active" href="#"><span class="fa-li"><i class="fas fa-user-edit"></i></span>Account Name</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-portrait"></i></span>Display Picture (dp)</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-image"></i></span>Cover Photo (cp)</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-newspaper"></i></span>Notice Board</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-pen-square"></i></span>School Introduction</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="fas fa-images"></i></span>Slide Bar (1)</a></li>
+                                <li><a href="#"><span class="fa-li"><i class="far fa-images"></i></span>Slide Bar (2)</a></li>
+                          </ul>
+                          </div>
+  </div>
+</section>
+<div class="content">
+  <div class="content-data">
+  <div class="content-in">
+    <div class="content-fix-head"><h1>Account Name</h1></div>
+    <div class="main-content">
+      <div class="card-body noticeboard">
+                <div class="header_message sec2wrap btnwap">
+                    <div class="card-wrap">
+                     <div class="card-message">
+                    <div class="message">
+                         <p>Giv-Sch<br>
+                        <span class="mintext">Unverified</span></p>
+                       </div>
+                  </div>
+                     <div class="card-option">
+                    <div class="sidebtn ">
+                         <ul>
+                        <li>
+                        <a href="">
+                          <label class="switch">
+                            <input type="checkbox">
+                            <span class="slider round"></span> </label>
+                          </a>
+                         </li>
+                        <li><a href="" class="refresh"><img src="images/icon14.png" class="iconback1"></a></li>
+                        <li><a href="" class="edit-btn"><img src="images/edit.png" class="iconback2"></a></li>
+                      </ul>
+                       </div>
+                  </div>
+                    </div>
+                   </div>
+                   <div class="header_message sec2wrap btnwap">
+                    <div class="card-wrap">
+                     <div class="card-message">
+                    <div class="message">
+                         <p>Sur-Abb<br>
+                        <span class="mintext">User</span></p>
+                       </div>
+                  </div>
+                     <div class="card-option">
+                    <div class="sidebtn ">
+                         <ul>
+                        <li>
+                        <a href="">
+                          <label class="switch">
+                            <input type="checkbox">
+                            <span class="slider round"></span> </label>
+                          </a>
+                         </li>
+                        <li><a href="" class="refresh"><img src="images/icon14.png" class="iconback1"></a></li>
+                        <li><a href="" class="edit-btn"><img src="images/edit.png" class="iconback2"></a></li>
+                      </ul>
+                       </div>
+                  </div>
+                    </div>
+                   </div>
+              </div>
+    </div>
+  </div>
+  </div>
+</div>
+</main>
+<?php include("footer.php"); ?>
+</body>
+</html>
